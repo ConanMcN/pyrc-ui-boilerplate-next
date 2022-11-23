c991feb7ee0004ddb10f4959c2cfe2d588c0e4ec
Conan McNicholl <mcn_marketing@outlook.com>
2022-11-23 12:05:37 +0000

    Making side nav collapsable

--- app/(components)/navigation.tsx
+++ app/(components)/navigation.tsx
@@ -1,40 +1,76 @@
+"use client";
+
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBookOpen,
+  faChartPie,
+  faCog,
+  faDatabase,
+  faHome,
+  faAngleDoubleLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import styles from "./styles.module.scss";
 import { ROUTES } from "types";
+import { Chevron } from "assets/icons";
 
 const exampleNavItems = [
   {
     label: "Home",
     path: ROUTES.HOME,
+    icon: <FontAwesomeIcon icon={faHome} width={16} />,
   },
   {
     label: "Articles",
     path: ROUTES.ARTICLE,
+    icon: <FontAwesomeIcon icon={faBookOpen} width={16} />,
   },
   {
     label: "Insights",
     path: ROUTES.HOME,
+    icon: <FontAwesomeIcon icon={faChartPie} width={16} />,
   },
   {
     label: "Admin",
     path: ROUTES.HOME,
+    icon: <FontAwesomeIcon icon={faDatabase} width={16} />,
   },
   {
     label: "Settings",
     path: ROUTES.HOME,
+    icon: <FontAwesomeIcon icon={faCog} width={16} />,
   },
 ];
 
-export function Navigation() {
+type NavigationProps = {
+  isOpen: boolean;
+  setIsOpen: () => void;
+};
+
+export function Navigation({ isOpen, setIsOpen }: NavigationProps) {
   return (
     <nav className={styles.navigation}>
       <ul>
-        {exampleNavItems.map(({ label, path }) => (
+        {exampleNavItems.map(({ label, path, icon }) => (
           <li key={label}>
-            <Link href={path}>{label}</Link>
+            <Link href={path}>
+              {icon}
+              {isOpen && <span>{label}</span>}
+            </Link>
           </li>
         ))}
+
+        <li
+          className={`${styles["navigation-expand"]} ${
+            !isOpen ? styles["menu-closed"] : ""
+          }`}
+          onClick={setIsOpen}
+        >
+          <FontAwesomeIcon icon={faAngleDoubleLeft} width={16} />
+          {/* {isOpen && <span>Collapse Menu</span>} */}
+        </li>
       </ul>
     </nav>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
 import styles from "./styles.module.scss";
 import "styles/globals.scss";
-
 import { Header, Footer, Navigation } from "./(components)";
 
 type RootLayoutProps = {
@@ -8,13 +11,23 @@ type RootLayoutProps = {
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+  function handleExpandOnClick() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <html lang="en">
       <body>
-        <div className={styles["root-layout"]}>
+        <div
+          className={`${styles["root-layout"]} ${
+            !isMenuOpen ? styles["menu-closed"] : ""
+          }`}
+        >
           <Header />
 
-          <Navigation />
+          <Navigation isOpen={isMenuOpen} setIsOpen={handleExpandOnClick} />
 
           <main className={styles["main-content"]}>
             {children}
